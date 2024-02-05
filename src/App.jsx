50d6ee6ed0647c32c4f1ff0a0923e507cd1a3762
davidho-o <146464286+davidho-o@users.noreply.github.com>
2024-02-05 13:08:50 +0000

    bines

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [posLeft,setPosLeft] = useState(600);
+  const [posTop,setPosTop] = useState(350);
+  const [yesScale,setYesScale] = useState (1);
+  const [text,setText] = useState ('');
+  function miscaButon(){
+    let newLeft = Math.random() * 650;
+    let newTop = Math.random() * 650;
+    setPosLeft(newLeft);
+    setPosTop(newTop);
+    setYesScale(yesScale+0.7)
+    setText('');
+  }
+  const normal = () => {
+    setYesScale(1);
+    setText('Yippieeeee');
+    setPosLeft(600);
+    setPosTop(350);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <br></br>
+      <h1 className='text'>Do you want to go on a date with me?</h1>
+      <div className='poza'><img src="bear.png" className='bear'></img></div>
+      <button className='buton1' style={{
+        transform: "scale("+ yesScale + ")",
+        position:'absolute',
+        top: 350,
+        left: 333
+      }}
+      onClick={normal}
+      >yes</button>
+      <button className='buton2' style={{
+        position:'absolute',
+        top: posTop,
+        left: posLeft
+      }}
+      onMouseEnter={miscaButon}>no</button>
+      <p className='yey'>{text}</p>
     </div>
   );
 }
